c4674723cf00bb4b9dec354d4947b3c2703876a4
Matt McClellan <mcclellan.mj@gmail.com>
2019-01-30 22:35:36 -0600

    Added the production build files

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,23 @@ gulp.task('static', function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('elm', function(){
+function buildElm(debug) {
   return gulp.src('src/elm/Main.elm')
-    .pipe(elm.bundle('elm.js', { debug: true }))
+    .pipe(elm.bundle('elm.js', { debug: debug }))
     .pipe(gulp.dest('dist/'));
+}
+
+gulp.task('elmDebug', function(){
+  return buildElm(true);
 });
 
-gulp.task('serve', gulp.series('static', 'elm', function() {
+gulp.task('elmBuild', function() {
+  return buildElm(false);
+});
+
+gulp.task('build', gulp.series('static', 'elmBuild'));
+
+gulp.task('serve', gulp.series('static', 'elmDebug', function() {
   browserSync.init({
     server: 'dist/',
     cors: true,
